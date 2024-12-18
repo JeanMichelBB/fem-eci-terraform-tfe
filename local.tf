337b3161d02a4ef465716f2928d990d0e2157d9e
JeanMichelBB <jeanmichelbberube@outlook.com>
2024-12-18 15:54:06 -0500

    feat: setup github workspace

--- local.tf
+++ local.tf
@@ -9,9 +9,13 @@ locals {
             description    = "example description for workspace"
             execution_mode = "remote"
             project_id     = module.project["fem-eci-project"].id
-            vcs_repo_identifier = "JeanMichelBB/fem-eci-terraform-tfe"
+            vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-tfe"
         }
-
+        "fem-eci-github" = {
+            description    = "example description for github resources"
+            execution_mode = "local"
+            project_id     = module.project["fem-eci-project"].id
+            vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-github"
+        }
     }
-
 }
