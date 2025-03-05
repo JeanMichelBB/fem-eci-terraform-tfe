c8397e71d61f6b68ba5d2c4721c4144f7e8be355
Jean-Michel Bérubé <107640908+JeanMichelBB@users.noreply.github.com>
2025-03-05 15:02:19 -0500

    feat: added workspaces (#4)

--- locals.tf
+++ locals.tf
@@ -72,6 +72,78 @@ locals {
         },
       ]
     }
+
+    "fem-eci-oci-network" = {
+      description         = "Automation for OCI network resources."
+      execution_mode      = "remote"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/oci-terraform-network"
+
+      variables = [
+        {
+          category = "terraform"
+          hcl      = true
+          key      = "availability_domains"
+          value    = jsonencode(["AD-1", "AD-2"]) # Update availability domains as per your OCI region
+        },
+        {
+          category = "terraform"
+          key      = "cidr"
+          value    = "10.0.0.0/16"
+        },
+        {
+          category = "terraform"
+          key      = "name"
+          value    = "fem-eci"
+        },
+        {
+          category = "terraform"
+          key      = "region"
+          value    = "ca-montreal-1"
+        }
+      ]
+    }
+
+    "fem-eci-oci-cluster" = {
+      description         = "Automation for OCI cluster resources."
+      execution_mode      = "remote"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/oci-terraform-cluster"
+
+      variables = [
+        {
+          category = "terraform"
+          key      = "domain"
+          value    = "altf4.dev"
+        },
+        {
+          category = "terraform"
+          key      = "environment"
+          value    = "prod"
+        },
+        {
+          category = "terraform"
+          key      = "name"
+          value    = "fem-eci-jmbberube"
+        },
+        {
+          category = "terraform"
+          key      = "vcn_name"
+          value    = "fem-eci-vcn"
+        },
+        {
+          category = "terraform"
+          key      = "subnet_name"
+          value    = "fem-eci-subnet"
+        },
+        {
+          category = "terraform"
+          key      = "compartment_id"
+          value    = "ocid1.compartment.oc1..aaaaaaaal6bbq6ianvrjr2dfygw3nxubpi6cn6fk5wesl7pbcn2py6s7u5dq"
+        }
+      ]
+    }
+
     "fem-eci-product-service-prod" = {
       description         = "Automation for product service resources."
       execution_mode      = "remote"
